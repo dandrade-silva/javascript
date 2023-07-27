9cb0febe3c95ebe7620765ffb2c76c4b77f4547b
Danilo Andrade <dandrade0803@gmail.com>
2023-07-26 21:56:28 -0300

    Jogo da cobrinha Manual do Dev

---
+++ extras/jogos/snake_game/js/script.js
@@ -0,0 +1,184 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext("2d")
+
+const audio = new Audio('../assets/audio.mp3')
+
+const size = 30
+
+const snake = [{ x: 270, y: 240 }]
+
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+const food = {
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
+
+}
+
+let direction
+let loopId
+
+const drawFood = () => {
+
+    const { x, y, color } = food
+    ctx.shadowColor = color
+    ctx.shadowBlur = 6
+    ctx.fillStyle = food.color
+    ctx.fillRect(food.x, food.y, size, size)
+    ctx.shadowBlur = 0
+
+}
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+
+    snake.forEach((position, index) => {
+        if (index == snake.length - 1) {
+            ctx.fillStyle = "white"
+        }
+
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+const moveSnake = () => {
+    if (!direction) return
+
+    const head = snake[snake.length - 1]
+
+    if (direction == "right") {
+        snake.push({ x: head.x + size, y: head.y })
+    }
+
+    if (direction == "left") {
+        snake.push({ x: head.x - size, y: head.y })
+    }
+
+    if (direction == "down") {
+        snake.push({ x: head.x, y: head.y + size })
+    }
+    if (direction == "up") {
+        snake.push({ x: head.x, y: head.y - size })
+    }
+    snake.shift() // remove o último elemento do array
+}
+
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+    }
+
+}
+
+const checkEat = () => {
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y) {
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (snake.find((position) => position.x == x && position.y == y)) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+
+    }
+}
+
+const checkCollision = () => {
+    const head = snake[snake.length - 1]
+    const canvasLimit = canvas.width - size
+
+    const wallCollision = head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    if (wallCollision) {
+        alert('Você perdeu')
+    }
+}
+
+const gameLoop = () => {
+    clearInterval(loopId)
+
+    ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
+    moveSnake()
+    drawSnake()
+    checkEat()
+    checkCollision()
+
+    loopId = setTimeout(() => {
+        gameLoop()
+    }, 600)
+}
+
+gameLoop()
+
+document.addEventListener("keydown", ({ key }) => {
+    console.log(key)
+    switch (key) {
+        case "ArrowRight":
+            if (direction == "left") {
+                break
+            } else {
+                direction = "right"
+                break
+            }
+        case "ArrowLeft":
+            if (direction == "right") {
+                break
+            } else {
+                direction = "left"
+                break
+            }
+        case "ArrowDown":
+            if (direction == "up") {
+                break
+            } else {
+                direction = "down"
+                break
+            }
+        case "ArrowUp":
+            if (direction == "down") {
+                break
+            } else {
+                direction = "up"
+                break
+            }
+            break
+    }
+})
